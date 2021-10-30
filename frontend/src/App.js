f5ad835d67681321d66603993a35d7c921129313
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-10-30 22:18:52 +0200

    display API data in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Posts from "./components/Posts";
+import PostLoadingComponent from "./components/PostLoading";
 
-class connectionExample extends React.Component {
-  componentDidMount() {
-    const apiUrl = 'http://127.0.0.1:8000/api/';
+const App = () => {
+  const PostLoading = PostLoadingComponent(Posts);
+  const [appState, setAppState] = useState({
+    loading: false,
+    posts: null,
+  });
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `http://127.0.0.1:8000/api/`;
     fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }
-  render() {
-    return <div>Example connection</div>
-  }
-}
+      .then((data) => data.json())
+      .then((posts) => {
+        setAppState({ loading: false, posts: posts });
+      });
+  }, [setAppState]); // TODO maybe put [] here?
+  return (
+    <div className="App">
+      <h1>Latest Posts</h1>
+      <PostLoading isLoading={appState.loading} posts={appState.posts} />
+    </div>
+  );
+};
 
-export default connectionExample;
-
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App;
