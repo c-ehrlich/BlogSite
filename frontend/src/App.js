d22fb4d7caed658f2746d29804cdc44c21ed42b3
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-10-30 20:57:29 +0200

    consume API from frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+class connectionExample extends React.Component {
+  componentDidMount() {
+    const apiUrl = 'http://127.0.0.1:8000/api/';
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+  render() {
+    return <div>Example connection</div>
+  }
+}
+
+export default connectionExample;
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  // This is basically the material UI equivalent of overriding
+  // a CSS class
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <CssBaseline />
+      <AppBar
+        position="static"
+        color="white"
+        elevation={0}
+        className={classes.appBar}
+      >
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            BlogmeUp
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Header;
