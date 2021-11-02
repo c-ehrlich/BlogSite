fa58e895edf3d9361d797355e0b92fb37c541951
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-11-02 14:48:40 +0100

    initial testing for authorization

--- backend/blog_api/tests.py
+++ backend/blog_api/tests.py
@@ -1,25 +1,98 @@
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 from blog.models import Post, Category
 from django.contrib.auth.models import User
 
 
 class PostTests(APITestCase):
     def test_view_post(self):
-        """view a post"""
+        """
+        Ensure we can view all objects
+        """
         url = reverse("blog_api:listcreate")
         response = self.client.get(url, format="json")
         self.assertEquals(response.status_code, status.HTTP_200_OK)
 
     def create_post(self):
-        """create a post"""
+        """
+        Ensure we can create a new Post object and view objects
+        """
         self.test_category = Category.objects.create(name="django")
         self.testuser1 = User.objects.create_user(
             username="test_user1", password="12345678"
         )
+
+        self.client.login(username=self.testuser.username, password="12345678")
+
         data = {"title": "new", "author": 1, "excerpt": "new", "content": "new"}
         url = reverse("blog_api:listcreate")
-        response = self.clent.post(url, data, format='json')
+        response = self.clent.post(url, data, format="json")
         self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+
+    def test_post_update(self):
+        """
+        Ensure that posts can not be edited by a user who is neither the author
+        nor a superuser
+        """
+        client = APIClient()
+
+        # there's probably a better way of doing this than to create new data
+        # for every test
+        self.test_category = Category.objects.create(name="django")
+        self.testuser1 = User.objects.create_user(
+            username="test_user1", password="123456789"
+        )
+        self.testuser2 = User.objects.create_user(
+            username="test_user2", password="123456789"
+        )
+        test_post = Post.objects.create(
+            category_id=1,
+            title="Post Title",
+            excerpt="Post Excerpt",
+            content="Post Content",
+            slug="post-title",
+            author_id=1,
+            status="published",
+        )
+
+        client.login(username=self.testuser1.username, password="123456789")
+
+        # navigate to the right endpoint
+        url = reverse(("blog_api:detailcreate"), kwargs={"pk": 1})
+
+        # create a PUT request
+        response = client.put(
+            url,
+            {
+                # id not required because it's in the URL
+                "title": "New",
+                "author": 1,
+                "excerpt": "New",
+                "content": "New",
+                "status": "published",
+            },
+            format="json",
+        )
+
+        # a good tool for debugging tests
+        print(response.data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        edited_post = Post.postobjects.get(id=1)
+        self.assertEqual(edited_post.title, "New")
+
+    # TODO a similar test that gets an error because a user is trying to edit
+    # a post that's not their own
+
+    # TODO a test where we try to make a post without being logged into an account
+
+    # TODO a test that looks for the error that you get when submitting
+    # incomplete data for editing
+
+    # TODO a test that looks at what happens when you try to submit a post title
+    # that's too long, either in Post Creation or in Post Editing
+
+    # TODO confirm that a superuser can edit someone else's post (is this true)
--- backend/blog_api/views.py
+++ backend/blog_api/views.py
@@ -1,15 +1,33 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import (
+    SAFE_METHODS,
+    BasePermission,
+    IsAdminUser,
+    DjangoModelPermissionsOrAnonReadOnly,
+)
+
+
+class PostUserWritePermission(BasePermission):
+    """Posts can only be edited or deleted by their author"""
+
+    message = "Editing posts is restricted to the author only."
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:  # GET, OPTIONS, HEAD
+            return True
+
+        return obj.author == request.user  # is the person trying to edit the author?
 
 
 class PostList(generics.ListCreateAPIView):
-    # permission_classes = [IsAdminUser]
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
     queryset = Post.postobjects.all()
     serializer_class = PostSerializer
 
 
-class PostDetail(generics.RetrieveDestroyAPIView): # TODO use RetrieveUpdateDestroy?
-    queryset = Post.objects.all() # not using postobjects on purpose!
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [PostUserWritePermission]
+    queryset = Post.objects.all()  # not using postobjects on purpose!
     serializer_class = PostSerializer
